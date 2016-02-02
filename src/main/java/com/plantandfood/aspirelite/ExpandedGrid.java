4ddf20cb81b9e0dd4f9784100b0b1adbeb41c3b0
Alastair Hughes <hobbitalastair@yandex.com>
2016-02-02 13:02:28 +1300

    Refactored - the scroll layout now surrounds everything...

---
+++ src/main/java/com/plantandfood/aspirelite/ExpandedGrid.java
@@ -0,0 +1,25 @@
+package com.plantandfood.aspirelite;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+public class ExpandedGrid extends GridView {
+
+    public ExpandedGrid(Context context) {
+        super(context);
+    }
+    public ExpandedGrid(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public ExpandedGrid(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int expandSpec = MeasureSpec.makeMeasureSpec(MEASURED_SIZE_MASK, MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+}
