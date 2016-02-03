61fbc80002872dc25ca5d75440b3717fd8391ec4
Alastair Hughes <hobbitalastair@yandex.com>
2016-02-03 15:29:47 +1300

    Added more detail about the limitations of the model and app

--- src/main/java/com/plantandfood/aspirelite/HelpActivity.java
+++ src/main/java/com/plantandfood/aspirelite/HelpActivity.java
@@ -2,6 +2,8 @@ package com.plantandfood.aspirelite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        /* Set the link method */
+        TextView aboutText = (TextView) findViewById(R.id.HelpLimitationsText);
+        aboutText.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
