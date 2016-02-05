832579a2ae9bf4ebe2ff4a6f498d37fa195734de
Alastair Hughes <hobbitalastair@yandex.com>
2016-02-05 13:11:17 +1300

    Ensured that the formatting of the license is preserved, and that the link works

--- src/main/java/com/plantandfood/aspirelite/LegalActivity.java
+++ src/main/java/com/plantandfood/aspirelite/LegalActivity.java
@@ -1,6 +1,8 @@
 package com.plantandfood.aspirelite;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class LegalActivity extends ChildActivity {
 
@@ -8,5 +10,9 @@ public class LegalActivity extends ChildActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_legal);
+
+        /* Set the link method */
+        TextView aboutText = (TextView) findViewById(R.id.LegalText);
+        aboutText.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
